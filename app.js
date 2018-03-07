c4e700714c32b6c635c57caa43eb81fe9bf57ab8
Doo <kde1690@gmail.com>
2018-03-07 15:11:45 -0500

    add cors

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors  = require('cors')
 const api = require('./routes/routesPost')
 const app = express()
 
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
+app.use(cors())
 
 //endpoints
 app.use('/api',api)
